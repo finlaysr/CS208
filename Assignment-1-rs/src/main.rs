3e254e18893172fbedfe178ddfcdea64542b1d2e
finlaysr <60317020+finlaysr@users.noreply.github.com>
2026-02-23 23:26:14 +0000

    Improved running

--- Assignment-1-rs/src/main.rs
+++ Assignment-1-rs/src/main.rs
@@ -5,31 +5,32 @@ mod sorting;
 use rand::prelude::*;
 
 fn main() {
-    for i in 1..1000 {
+    for i in (0..=100_000).step_by(10000) {
         run_merge_sort(i);
     }
-    run_merge_sort(200000);
-    run_selection_sort(200000);
+    for i in (0..=100_000).step_by(10000) {
+        run_selection_sort(i);
+    }
 }
 
 fn run_selection_sort(size: u32) {
     let mut rng = rand::rng();
-    let mut arr: Vec<u32> = vec![];
+    let mut arr = vec![];
     for _ in 0..size {
-        arr.push(rng.random_range(0..=100));
+        arr.push(rng.random_range(-1000..=1000));
     }
     sorting::selection_sort(&mut arr);
-    println!("{:04}. Sorted: {}", size, arr.is_sorted());
+    println!("{:04}. Selection Sorted: {}", size, arr.is_sorted());
 }
 
 fn run_merge_sort(size: u32) {
     let mut rng = rand::rng();
-    let mut arr: Vec<u32> = vec![];
+    let mut arr = vec![];
     for _ in 0..size {
-        arr.push(rng.random_range(0..=100));
+        arr.push(rng.random_range(-1000..=1000));
     }
     sorting::merge_sort(&mut arr);
-    println!("{:04}. Sorted: {}", size, arr.is_sorted());
+    println!("{:04}. Merge Sorted: {}", size, arr.is_sorted());
 }
 
 #[cfg(test)]
